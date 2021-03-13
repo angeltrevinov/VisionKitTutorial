5350dbd5f6f4df709a6f3e94bbb44b497df83ac3
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2021-03-12 18:34:31 -0600

    tutorial

---
+++ VisionKitTutorial/ScanButton.swift
@@ -0,0 +1,32 @@
+//
+//  ScanButton.swift
+//  VisionKitTutorial
+//
+//  Created by Angel Trevino on 11/03/21.
+//
+
+import UIKit
+
+class ScanButton: UIButton {
+    
+    // -----------------------------------------------------
+    override init(frame: CGRect) {
+        super.init(frame: frame);
+        configure();
+    }
+    // -----------------------------------------------------
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented");
+    }
+    // -----------------------------------------------------
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false;
+        setTitle("Scan", for: .normal);
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: 18.0);
+        titleLabel?.textColor = .white;
+        layer.cornerRadius = 7.0;
+        backgroundColor = UIColor.systemIndigo;
+        
+    }
+    
+}
